a30355a33ff8708e55bcc3cffeb0d8e32638cb3a
onionbloom <abdurro.muhammad@gmail.com>
2020-11-19 16:01:17 +0700

    More Form Updates

--- toolbox/forms.py
+++ toolbox/forms.py
@@ -13,22 +13,22 @@ ALLOWED_EXTENSIONS = ['xls', 'xlsx', 'csv', 'txt']
 class RegistrationForm(FlaskForm):
     """ Flask Form to handle user registration """
     username = StringField('Username', validators=[
-        DataRequired(), Length(min=5, max=20)])
-    email = StringField('User Email', validators=[DataRequired(), Email()])
+        DataRequired(), Length(min=5, max=20)], render_kw={"autocomplete": "off"})
+    email = StringField('User Email', validators=[DataRequired(), Email()], render_kw={"autocomplete": "off"})
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up', id='buttonRegister')
 
     def validate_username(self, username):
-        """ This custom validator checks if the input username already exists """
+        """ This custom in-line validator checks if the input username already exists """
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError(
                 'That username is already taken. Please choose another username.')
 
     def validate_email(self, email):
-        """ This custom validator checks if the input email address already exists """
+        """ This custom in-line validator checks if the input email address already exists """
         email = User.query.filter_by(email=email.data).first()
         if email:
             raise ValidationError(
